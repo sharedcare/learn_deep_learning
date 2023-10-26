05dfe094779680f958f9a208a747f97a833845e3
yizhen-chen <sharedcare@hotmail.com>
2023-10-26 20:40:59 +0800

    Added training procedure for dqn

--- reinforcement learning/dqn.py
+++ reinforcement learning/dqn.py
@@ -10,6 +10,8 @@ from torch import Tensor
 from collections import deque, namedtuple
 from typing import List, Tuple, Optional
 
+from utils.device import get_device
+
 
 class ReplayBuffer(object):
     Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state'))
@@ -50,9 +52,12 @@ class DQN:
                  num_states: int,
                  num_actions: int,
                  hidden_dim: int,
+                 eps_start: float,
+                 eps_end: float,
+                 eps_decay: int,
                  mem_capacity: int,
                  batch_size: int,
-                 device: str = "cpu") -> None:
+                 device: torch.device = "cpu") -> None:
         super(DQN, self).__init__()
         self.env = gym.make("cartpole")
         self.policy_net = Net(num_states, num_actions, hidden_dim)
@@ -62,13 +67,17 @@ class DQN:
         self.batch_size = batch_size
         self.device = device
 
+        self.eps_start = eps_start
+        self.eps_end = eps_end
+        self.eps_decay = eps_decay
+
         self.replay_buffer = ReplayBuffer(mem_capacity)
         self.optimizer = optim.Adam(self.policy_net.parameters(), lr=1e-3)
 
         self.steps_done = 0
 
     def act(self, obs) -> Tensor:
-        epsilon = eps_end + (eps_start - eps_end) * math.exp(-1 * self.steps_done / eps_decay)
+        epsilon = self.eps_end + (self.eps_start - self.eps_end) * math.exp(-1 * self.steps_done / self.eps_decay)
         if random.random() > epsilon:
             with torch.no_grad():
                 action_value = self.policy_net(obs)
@@ -97,6 +106,7 @@ class DQN:
     def update(self):
         if len(self.replay_buffer) < self.mem_capacity:
             return
+        # randomly sample from replay buffer
         sample = self.replay_buffer.sample(self.batch_size)
         batch = self.replay_buffer.Transition(*zip(*sample))
 
@@ -109,6 +119,18 @@ class DQN:
         state_action_values = self.policy_net(state_batch).gather(1, action_batch)
 
         # V(s_{t+1}): \max_a Q(s_{t+1}, a)
+        next_state_values = torch.max(self.target_net(non_terminal_next_state_batch), 1)[0]
+
+        # Expected Q(s,a): r + \gamma V(s_{t+1}) using Bellman equation
+        expected_state_action_values = reward_batch + gamma * next_state_values
+
+        # TD error: Q(s_t, a) - Expected Q(s, a)
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
     def learn(self, num_episodes):
         for i in range(num_episodes):
@@ -118,3 +140,37 @@ class DQN:
             while not done:
                 next_state, action, reward, done = self.step(state)
                 episode_rewards += reward
+                self.update()
+
+            print("step: {}, rew: {}".format(i + 1, episode_rewards))
+
+
+if __name__ == "__main__":
+    EPSILON_START = 0.9         # start epsilon for greedy policy
+    EPSILON_END = 0.05          # minimum epsilon
+    EPSILON_DECAY = 1000        # decay rate from epsilon
+    BATCH_SIZE = 32             # sample batch size
+    GAMMA = 0.99                # reward discount
+    LR = 1e-3                   # learning rate
+    MEMORY_CAPACITY = 10000     # replay buffer memory capacity
+    NUM_EPISODES = 500          # number of episodes for sampling and training
+    HIDDEN_DIM = 128            # hidden dimension size for Q-network
+
+    env = gym.make("CartPole-v1").unwrapped
+    n_states = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+
+    run_device = get_device()
+
+    dqn = DQN(num_states=n_states,
+              num_actions=n_actions,
+              hidden_dim=HIDDEN_DIM,
+              eps_start=EPSILON_START,
+              eps_end=EPSILON_END,
+              eps_decay=EPSILON_DECAY,
+              mem_capacity=MEMORY_CAPACITY,
+              batch_size=BATCH_SIZE,
+              device=run_device,
+              )
+
+    dqn.learn(NUM_EPISODES)
